1940f3062050d166029e8b344ca80515a06a21a7
aofleejay <aofleejay@hotmail.com>
2018-07-05 11:31:48 +0700

    Add Automatic Persisted Queries

--- client/src/lib/apollo.js
+++ client/src/lib/apollo.js
@@ -1,10 +1,12 @@
+import { createPersistedQueryLink } from 'apollo-link-persisted-queries'
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
+import { createHttpLink } from 'apollo-link-http'
 import { SERVICE_ENDPOINT } from '../config'
 
+const link = createPersistedQueryLink().concat(createHttpLink({ uri: SERVICE_ENDPOINT }))
 const client = new ApolloClient({
-  link: new HttpLink({ uri: SERVICE_ENDPOINT }),
+  link,
   cache: new InMemoryCache(),
 })
 
--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,10 @@
-import express from 'express';
-import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import { ApolloEngine } from 'apollo-engine';
-import schema from './schemas';
+import express from 'express'
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import { ApolloEngine } from 'apollo-engine'
+import schema from './schemas'
 import {
   DB_HOST,
   DB_NAME,
@@ -12,41 +12,46 @@ import {
   DB_PASSWORD,
   NODE_PORT,
   APOLLO_ENGINE_API_KEY,
-} from './config';
+} from './config'
 
-const uri = DB_HOST;
+const uri = DB_HOST
 const options = {
   dbName: DB_NAME,
   auth: {
     user: DB_USER,
     password: DB_PASSWORD,
   },
-};
+}
 
 mongoose.connect(uri, options).then(
   () => console.log('Connected to database.'),
   err => console.error('Failed to connect database', err),
-);
+)
 
-const app = express();
+const app = express()
 
-app.use(cors());
+app.use(cors())
 app.use('/graphql', bodyParser.json(), graphqlExpress({
   schema,
   tracing: true,
   cacheControl: {
     defaultMaxAge: 3600,
   },
-}));
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+}))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
 const engine = new ApolloEngine({
   apiKey: APOLLO_ENGINE_API_KEY,
-});
+  frontends: [{
+    overrideGraphqlResponseHeaders: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  }],
+})
 
 engine.listen({
   port: NODE_PORT,
   expressApp: app,
 }, () => {
-  console.log(`http://localhost:${NODE_PORT}/graphiql`);
-});
+  console.log(`http://localhost:${NODE_PORT}/graphiql`)
+})
